00a376e2b13d7389b5f31f86b069ad1da0f612ca
havefun02 <lapphan290802@gmail.com>
2022-11-20 17:51:02 +0700

    done with factor fe

---
+++ src/fe/src/component/imgoverlay.js
@@ -0,0 +1,21 @@
+import './imgoverlay.css';
+export default function ImgOverlay({ props }) {
+  const [access, setAccess] = props;
+  return (
+    <div className="imgoverlay">
+      <div onClick={() => {}} className="imgoverlay-flex-box">
+        <div className="imgoverlay-background">
+          <img src={require('../assets/overlay.jpg')}></img>
+        </div>
+        <div
+          className="imgoverlay-exit"
+          onClick={() => {
+            setAccess((access) => !access);
+          }}
+        >
+          <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAAAjklEQVRIie2Vyw2AIBQER3uQaIlarhwsRy+Y4AfCPuTmnEx0dwg+FH4MzIAz5FzIZlmAHfCixIXMHjqSDMAaHtyAqaD8nhnVQE4ilysSc3mJpLo8J/ms/CSeEH+7tozzK/GqpZX3FdKuInuh6Ra9vVDLYSwuT92TJSWjaJYocy5LLIdIkjT/XEPjH87PgwNng1K28QMLlAAAAABJRU5ErkJggg==" />
+        </div>
+      </div>
+    </div>
+  );
+}
