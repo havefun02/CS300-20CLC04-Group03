f7708e289fca0803ffedb55d04dd248753fb4b78
20127049-NguyenDucMinh <81550231+20127049-NguyenDucMinh@users.noreply.github.com>
2022-11-21 14:11:59 +0700

    MinhPhotoAnimation

---
+++ src/fe/src/component/photo_animation.js
@@ -0,0 +1,9 @@
+export default function photo_animation() {
+    return (
+        <div className = "parent">
+            <div className =" child bg">
+                <a href = "#">Impossible is Nothing</a>
+            </div>
+        </div>
+    )
+}
